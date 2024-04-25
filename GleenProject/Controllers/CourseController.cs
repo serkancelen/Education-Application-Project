e37974dbf8bc63b7a449302296738380f88e479b
Serkan Celen <serkancelen@outlook.com.tr>
2024-04-25 13:48:14 +0300

    Add project files.

---
+++ GleenProject/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using Gleen.Models;
+using GleenProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gleen.Controllers
+{
+	public class CourseController : Controller
+	{
+		public IActionResult Index()
+		{
+			var model = Repository.Applications;
+			return View(model);
+		}
+		public IActionResult Apply()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Apply([FromForm]Candidate model)
+		{
+			if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+			{
+				ModelState.AddModelError("","There is already application for you.");
+			}
+			if (ModelState.IsValid)
+			{
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+			return View();
+			
+		}
+	}
+}
